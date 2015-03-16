694551e5486c9a10934e60bd7e32aba212fe5a81
tglasser15 <tglasser15@my.whitworth.edu>
2015-03-15 19:30:50 -0700

    register logic (in progress)
    we need to match consistencies between user submission and database. We
    need to decided if it is necessary to have a 'birthday' field for the
    coach

--- js/controllers/loginController.js
+++ js/controllers/loginController.js
@@ -1,5 +1,6 @@
 soccerStats.controller('loginController',
     function loginController($scope) {
+        //user login
         $scope.user = {name: '', password: ''};
 
         $scope.login = function(user) {
@@ -9,6 +10,36 @@ soccerStats.controller('loginController',
                 },
                 error: function(user, error) {
                     alert("failed to login");
+                }
+            });
+        };
+
+        //user register
+        $scope.newUser = {
+            name: '',
+            email: '',
+            password: '',
+            phone: '',
+            city: '',
+            state: ''
+        };
+
+        $scope.register = function (newUser) {
+            var registerUser = new Parse.User();
+            registerUser.set("name", newUser.name);
+            registerUser.set("email", newUser.email);
+            registerUser.set("password", newUser.password);
+            registerUser.set("phone", newUser.phone);
+            registerUser.set("city", newUser.city);
+            registerUser.set("state", newUser.state);
+            registerUser.set("birthday", newUser.birthday);
+
+            user.signUp(null, {
+                success: function (registerUser) {
+                    alert("registration successful");
+                },
+                error: function (registerUser, error) {
+                    alert("Error: " + error.code + " " + error.message);
                 }
             });
         };
