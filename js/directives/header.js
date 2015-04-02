3ac70238eda383f8ced38dab74fc6a9764cef04a
Alec Moore <amoore@sixthavestudios.com>
2015-04-01 21:46:54 -0700

    app header

--- js/directives/header.js
+++ js/directives/header.js
@@ -3,6 +3,16 @@ soccerStats.directive('header', function () {
         restrict: 'E',
         templateUrl: "./templates/directives/header.html",
         controller: function($scope){
+            $scope.showTeams = false;
+            $scope.showAccount = false;
+
+            $scope.toggleTeams = function() {
+                $scope.showTeams = !$scope.showTeams;
+            }
+
+            $scope.toggleAccount = function() {
+                $scope.showAccount = !$scope.showAccount;
+            }
 
         }
     };
