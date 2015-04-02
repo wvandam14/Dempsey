59fb22a3cdea9afdad1900fa714e1c14c3b98bd1
William Van Dam <wvandam14@my.whitworth.edu>
2015-04-02 14:42:34 -0700

    added a couple files

---
+++ js/directives/schedule.js
@@ -0,0 +1,9 @@
+soccerStats.directive('schedule', function () {
+    return {
+        restrict: 'E',
+        templateUrl: "./templates/directives/schedule.html",
+        controller: function($scope){
+            
+        }
+    };
+});
